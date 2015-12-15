8a857efba27d3bc87ba622f5b9512ed4c083b5b5
0wangxiaowei <2456897876@qq.com>
2015-12-15 14:59:51 +0800

    Initial Commit

---
+++ WCustomNavbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCustomNavbar
+//
+//  Created by apple on 15/12/15.
+//  Copyright © 2015年 xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
